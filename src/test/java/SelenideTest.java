ce8c7f2394dfad7ce6f20271850a0b0feec26d1b
Vlad Kashkarov <kwa-111@yandex.ru>
2024-11-13 17:25:13 +0300

    add: test Steps

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    private static final String REPOSITORY = "vldka/demoqa-test-30";
+
+    @Test
+    @DisplayName("Проверка на чистом Selenide")
+    public void testIssueSearchTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys(REPOSITORY);
+        $("#query-builder-test").submit();
+
+        $(linkText(REPOSITORY)).click();
+        $("#issues-tab").shouldHave(text("Issues"));
+    }
+
+    @Test
+    @DisplayName("Проверка по Steps Lambda")
+    public void testIssueSearchLambdaStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу", () -> open("https://github.com"));
+        step("Поиск" + REPOSITORY, () -> {
+            $(".search-input").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Клик репозитория" + REPOSITORY, () -> $(linkText(REPOSITORY)).click());
+        step("Проверяем наличие вкладки Issue", () -> {
+            $("#issues-tab").shouldHave(text("Issues"));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка по анотации @Steps")
+    public void testIssueSearchAnnotationStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.shouldSeeIssueWith("Issue");
+    }
+
+}
---
+++ src/test/java/WebSteps.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+
+    @Step("Проверяем наличие вкладки {text}")
+    public void shouldSeeIssueWith(String text) {
+        $("#issues-tab").shouldHave(text(text));
+    }
+}
